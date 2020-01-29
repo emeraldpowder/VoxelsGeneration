6a21bf7bebcf4417c875ead4896ccc6e77ba5f3f
Glavak <gau-gau@ya.ru>
2020-01-29 12:42:14 +0700

    Tile color getting & greedy algo

---
+++ Assets/VoxelTile.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoxelTile : MonoBehaviour
+{
+    public float VoxelSize = 0.1f;
+    public int TileSideVoxels = 8;
+
+    [HideInInspector] public byte[] ColorsRight;
+    [HideInInspector] public byte[] ColorsForward;
+    [HideInInspector] public byte[] ColorsLeft;
+    [HideInInspector] public byte[] ColorsBack;
+
+    public void CalculateSidesColors()
+    {
+        ColorsRight = new byte[TileSideVoxels * TileSideVoxels];
+        ColorsForward = new byte[TileSideVoxels * TileSideVoxels];
+        ColorsLeft = new byte[TileSideVoxels * TileSideVoxels];
+        ColorsBack = new byte[TileSideVoxels * TileSideVoxels];
+        
+        for (int y = 0; y < TileSideVoxels; y++)
+        {
+            for (int i = 0; i < TileSideVoxels; i++)
+            {
+                ColorsRight[y * TileSideVoxels + i] = GetVoxelColor(y, i, Vector3.right);
+                ColorsForward[y * TileSideVoxels + i] = GetVoxelColor(y, i, Vector3.forward);
+                ColorsLeft[y * TileSideVoxels + i] = GetVoxelColor(y, i, Vector3.left);
+                ColorsBack[y * TileSideVoxels + i] = GetVoxelColor(y, i, Vector3.back);
+            }
+        }
+    }
+
+    private byte GetVoxelColor(int verticalLayer, int horizontalOffset, Vector3 direction)
+    {
+        var meshCollider = GetComponentInChildren<MeshCollider>();
+
+        float vox = VoxelSize;
+        float half = VoxelSize / 2;
+
+        Vector3 rayStart;
+        if (direction == Vector3.right)
+        {
+            rayStart = meshCollider.bounds.min +
+                       new Vector3(-half, 0, half + horizontalOffset * vox);
+        }
+        else if (direction == Vector3.forward)
+        {
+            rayStart = meshCollider.bounds.min +
+                       new Vector3(half + horizontalOffset * vox, 0, -half);
+        }
+        else if (direction == Vector3.left)
+        {
+            rayStart = meshCollider.bounds.max +
+                       new Vector3(half, 0, -half - (TileSideVoxels - horizontalOffset - 1) * vox);
+        }
+        else if (direction == Vector3.back)
+        {
+            rayStart = meshCollider.bounds.max +
+                       new Vector3(-half - (TileSideVoxels - horizontalOffset - 1) * vox, 0, half);
+        }
+        else
+        {
+            throw new ArgumentException("Wrong direction value, should be Vector3.left/right/back/forward",
+                nameof(direction));
+        }
+
+        rayStart.y = meshCollider.bounds.min.y + half + verticalLayer * vox;
+
+        Debug.DrawRay(rayStart, direction * .1f, Color.blue, 2);
+
+        if (Physics.Raycast(new Ray(rayStart, direction), out RaycastHit hit, vox))
+        {
+            Mesh mesh = meshCollider.sharedMesh;
+
+            int hitTriangleVertex = mesh.triangles[hit.triangleIndex * 3];
+            byte colorIndex = (byte) (mesh.uv[hitTriangleVertex].x * 256);
+            return colorIndex;
+        }
+
+        return 0;
+    }
+}
---
+++ Assets/VoxelTilePlacer.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class VoxelTilePlacer : MonoBehaviour
+{
+    public VoxelTile[] TilePrefabs;
+    public Vector2Int MapSize = new Vector2Int(10, 10);
+
+    private VoxelTile[,] spawnedTiles;
+
+    private void Start()
+    {
+        spawnedTiles = new VoxelTile[MapSize.x, MapSize.y];
+
+        foreach (VoxelTile tilePrefab in TilePrefabs)
+        {
+            tilePrefab.CalculateSidesColors();
+        }
+
+        StartCoroutine(Generate());
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            StopAllCoroutines();
+
+            foreach (VoxelTile spawnedTile in spawnedTiles)
+            {
+                if (spawnedTile != null) Destroy(spawnedTile.gameObject);
+            }
+
+            StartCoroutine(Generate());
+        }
+    }
+
+    public IEnumerator Generate()
+    {
+        for (int x = 1; x < MapSize.x - 1; x++)
+        {
+            for (int y = 1; y < MapSize.y - 1; y++)
+            {
+                yield return new WaitForSeconds(0.02f);
+
+                PlaceTile(x, y);
+            }
+        }
+        
+        yield return new WaitForSeconds(0.8f);
+        foreach (VoxelTile spawnedTile in spawnedTiles)
+        {
+            if (spawnedTile != null) Destroy(spawnedTile.gameObject);
+        }
+
+        StartCoroutine(Generate());
+    }
+
+    private void PlaceTile(int x, int y)
+    {
+        List<VoxelTile> availableTiles = new List<VoxelTile>();
+
+        foreach (VoxelTile tilePrefab in TilePrefabs)
+        {
+            if (CanAppendTile(spawnedTiles[x - 1, y], tilePrefab, Vector3.left) &&
+                CanAppendTile(spawnedTiles[x + 1, y], tilePrefab, Vector3.right) &&
+                CanAppendTile(spawnedTiles[x, y - 1], tilePrefab, Vector3.back) &&
+                CanAppendTile(spawnedTiles[x, y + 1], tilePrefab, Vector3.forward))
+            {
+                availableTiles.Add(tilePrefab);
+            }
+        }
+
+        if (availableTiles.Count == 0) return;
+
+        VoxelTile selectedTile = availableTiles[Random.Range(0, availableTiles.Count)];
+        Vector3 position = selectedTile.VoxelSize * selectedTile.TileSideVoxels * new Vector3(x, 0, y);
+        spawnedTiles[x, y] = Instantiate(selectedTile, position, selectedTile.transform.rotation);
+    }
+
+    private bool CanAppendTile(VoxelTile existingTile, VoxelTile tileToAppend, Vector3 direction)
+    {
+        if (existingTile == null) return true;
+
+        if (direction == Vector3.right)
+        {
+            return Enumerable.SequenceEqual(existingTile.ColorsRight, tileToAppend.ColorsLeft);
+        }
+        else if (direction == Vector3.left)
+        {
+            return Enumerable.SequenceEqual(existingTile.ColorsLeft, tileToAppend.ColorsRight);
+        }
+        else if (direction == Vector3.forward)
+        {
+            return Enumerable.SequenceEqual(existingTile.ColorsForward, tileToAppend.ColorsBack);
+        }
+        else if (direction == Vector3.back)
+        {
+            return Enumerable.SequenceEqual(existingTile.ColorsBack, tileToAppend.ColorsForward);
+        }
+        else
+        {
+            throw new ArgumentException("Wrong direction value, should be Vector3.left/right/back/forward",
+                nameof(direction));
+        }
+    }
+}
